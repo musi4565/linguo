8991917dc760aa291ac8189092f5c05c009ec383
Muslima <emailing@gmail.com>
2026-08-25 17:34:26 +0300

    fix: serve media files in production when DEBUG=False

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,9 @@
-import json
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from apps.users.views import ChangePasswordView, ProfileView
 
@@ -25,4 +24,9 @@ urlpatterns = [
     path("api/", include("apps.achievements.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
